65a5ab55d9b054493135f01ce285a06bd307bd8e
Franklin J. Casco <kodeandoec@gmail.com>
2023-08-09 15:33:05 -0500

    add styles file

--- App.tsx
+++ App.tsx
@@ -2,24 +2,17 @@ import React from 'react';
 import { 
   View,
   Text,
-  StyleSheet
 } from 'react-native';
+
+import styles from './styles';
 
 const App = () => {
 
   return(
     <View style={ styles.container }>
-      <Text>Hola Mundo</Text>
+      <Text style={styles.text}>Hola Mundo</Text>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
 
 export default App;
